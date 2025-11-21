0ce687243713716ba7a24e0c96d7866ddb425a9c
Vaclav <vaclav.pech@gmail.com>
2025-11-21 10:39:11 +0100

    Minor updates for the conncurrency lecture

--- lecture1124/exercises/07_concurrency/G20_ReactiveAndStructured.groovy
+++ lecture1124/exercises/07_concurrency/G20_ReactiveAndStructured.groovy
@@ -2,44 +2,62 @@ import groovyx.gpars.dataflow.DataflowVariable
 import groovyx.gpars.dataflow.DataflowQueue
 import groovyx.gpars.dataflow.DataflowBroadcast
 import groovyx.gpars.group.NonDaemonPGroup
+import groovyx.gpars.group.DefaultPGroup
 import groovyx.gpars.group.PGroup
 
 
-def group = new NonDaemonPGroup()
-def stockPrices = new DataflowQueue()
-def exchangeRates = new DataflowQueue()
+def group1 = new NonDaemonPGroup(4)
+def group2 = new DefaultPGroup()
+def stockPricesPRG = new DataflowQueue()
+def stockPricesVIE = new DataflowQueue()
+def stockPricesSTG = new DataflowQueue()
+def avgPrices = new DataflowQueue()
+
 def exit = new DataflowVariable()
 
-group.task {
+def prg = group1.task {
     def random = new Random()
     10.times {
         sleep 1000
-        stockPrices << random.nextInt(10)
+        stockPricesPRG << random.nextInt(10)
     }
-    exit << true
+}
+def vie = group1.task {
+    def random = new Random()
+    10.times {
+        sleep 1000
+        stockPricesVIE << random.nextInt(10)
+    }
+}
+def stg = group1.task {
+    def random = new Random()
+    10.times {
+        sleep 1000
+        stockPricesSTG << random.nextInt(10)
+    }
 }
 
-group.task {
-        exchangeRates << 25
-        sleep 3000
-        exchangeRates << 24
-        sleep 3000
-        exchangeRates << 23                
+def average = group2.operator(inputs: [stockPricesPRG, stockPricesVIE, stockPricesSTG], outputs: [avgPrices]) {p, v, s ->
+        def avg = (p+v+s)/3
+        println "Calculating the average price for: $p $v $s"
+        bindOutput(0, avg)
 }
 
 
 //Handle progress
-group.task {
-    def alt = group.select(exchangeRates, stockPrices, exit)
-    int currentRate = 0
+group2.task {
+    def alt = group2.select(avgPrices, exit)
     while(true) {
         def msg = alt.prioritySelect()
-        if (msg.index == 2) break
+        if (msg.index == 1) break
         if (msg.index == 0) {
-            currentRate = msg.value
-            println "Exchange rate changed to ${currentRate} CZK/EUR"
-        } else {
-            println "Current value of the stock ${msg.value} EUR, which is ${msg.value * currentRate} CZK"
+            println "Current value of the stock: ${msg.value} EUR\n"
+            sleep 1500  //Simulate the time penalty of a computation
         }
     }
-}.join()
+}
+
+[prg, vie, stg]*.join()
+sleep 3000
+exit << true
+println 'Done'
